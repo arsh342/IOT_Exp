abfc931818e155b2ee7e3cc464c399064d3da90d
arsh <135307874+arsh342@users.noreply.github.com>
2024-09-09 23:05:18 +0530

    Update Exp_7_Servo.ino

--- Exp_7_Servo.ino
+++ Exp_7_Servo.ino
@@ -1,11 +1,22 @@
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+                // twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
 void setup() {
-  pinMode(6, OUTPUT); // Set pin 6 as an output
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
 }
 
 void loop() {
-  digitalWrite(6, HIGH); // Turn on the LED (assuming pin 6 is connected to an LED)
-  delay(2000);           // Wait for 2 seconds
-
-  digitalWrite(6, LOW);  // Turn off the LED
-  delay(2000);           // Wait for 2 seconds
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
 }
